f74781234d450a12bde2eaae57ddfa1f0227d891
mayra manzaneda <urielortega300@gmail.com>
2024-05-11 16:19:30 -0300

    semillero Back

--- G202402_Ecosistema_001_front/src/utils/decodeJWT.jsx
+++
@@ -1,6 +0,0 @@
-import { jwtDecode } from "jwt-decode";
-
-export function decodeJWT({ token }) {
-  const decodedToken = jwtDecode(token);
-  return decodedToken;
-}
--- G202402_Ecosistema_001_front/src/utils/services/constants.js
+++
@@ -1,5 +0,0 @@
-// URL del backend local
-export const LOCAL_URL = 'http://localhost:8080';
-
-// URL del backend que luego proporcionaremos
-export const TESTING_URL = 'http://t....';
